0c8cf7dc20a80fe2ae4b3052d07d342b94c3cc26
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-06 18:48:57 +0530

    Routes Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Home from './home/Home';
 
+import { Routes, Route } from "react-router-dom";
+import Courses from './courses/Courses';
+
+
 function App() {
   return (
     <>
-    <Home/>
-    
+     <Routes>
+      <Route path = "/" element = {<Home/>}/>
+      <Route path = "/course" element={<Courses/>}/>
+     </Routes>
     </>
   )
 }
