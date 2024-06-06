c53822726672b4ac30e3f9f859cb2669aa91bffa
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-06 20:10:15 +0530

    Login component added

--- Frontend/src/components/Navbar.jsx
+++ Frontend/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Login from './Login';
 
 function Navbar() {
 
@@ -84,7 +85,8 @@ function Navbar() {
 </label>
 
   <div className="">
-    <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer">Login</a>
+    <a className="bg-black text-white px-3 py-2 rounded-md hover:bg-slate-800 duration-300 cursor-pointer "onClick={() =>document.getElementById("my_modal_3").showModal()}>Login</a><Login/>
+
   </div>
 </div>
 </div>
