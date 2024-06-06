87ebce3e0520ef388dd31c3657f6e2b0f1405d71
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-06 18:06:26 +0530

    Courses Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Banner from './components/Banner';
 import Footer from './components/Footer';
 import Freebook from './components/Freebook';
+import Course from './components/Course';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Banner/>
     <Freebook/>
     <Footer/>
+    <Course/>
     
     </>
   )
