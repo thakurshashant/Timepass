0ae75785a03ddd2a4501c1389719a9cb1146c5ad
deathnote <127240347+thakurshashant@users.noreply.github.com>
2024-06-11 15:54:36 +0530

    login button added

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -10,8 +10,28 @@ function Login() {
       {/* if there is a button in form, it will close the modal */}
       <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
     </form>
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click on ✕ button to close</p>
+    <h3 className="font-bold text-lg">Login</h3>
+    {/* emial */}
+    <div className="mt-4 space-y-2">
+      <span>Email</span>
+      <br />
+      <input type="email"
+      placeholder="Enter your email"  className="w-80 px-3 py-1 border rounded-md outline-none"/>
+      
+    </div>
+    {/* password div */}
+    <div className="mt-4 space-y-2">
+      <span>Password</span>
+      <br />
+      <input type="text"
+      placeholder="Enter your password"  className="w-80 px-3 py-1 border rounded-md outline-none"/>
+      
+    </div>
+    {/* button */}
+    <div className="flex justify-around mt-4">
+      <button className="bg-pink-500 text-white rounded-md px-3 py-1 hover:bg-pink-700 duration-200"> Login</button>
+      <p>Not registered? <span className="underline text-blue-500 cursor-pointer">Signup</span></p>
+      </div>
   </div>
 </dialog>
     </div>
